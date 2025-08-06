4514d294cebc3c95c324b40be3bbffce595ad244
aksharabandaru <aksharabandaru58@gmail.com>
2025-08-06 10:46:12 +0530

    Initial commit

---
+++ nex.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+// import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        // String a=sc.nextLine().charAt(0);
+        // System.out.println(a);
+        int x=5;
+        int y=x--;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
